5ffe0ae4a66a3bb1ec436ec1ccc57784ad6c26ed
neel32 <boghanineel@gmail.com>
2023-06-07 17:41:15 +0530

    pipeline

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,5 +2,21 @@
 
 from django.http import  HttpResponse
 
+# def index(request):
+#     return HttpResponse('''<h2>Hello World</h2>
+#     <a href="https://www.youtube.com/watch?v=oXN2q7MjxGo">Song Time</a>''')
+def about(request):
+    return HttpResponse(" About Hello World")
+
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Home")
+def removepunc(request):
+    return HttpResponse(" Remove Punc")
+def capfirst(request):
+    return HttpResponse("Capitalize First")
+def newlineremove(request):
+    return HttpResponse("New Line Remove")
+def spacefirst(request):
+    return HttpResponse("Space First")
+def charcount(request):
+    return HttpResponse("Char Counnt")
