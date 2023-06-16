271f974ab3e01c055bfda9a23fc3c7e2d4d5867c
neel32 <boghanineel@gmail.com>
2023-06-16 08:47:11 +0530

    index

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,22 +1,23 @@
 # This file add by me = Neel
 
 from django.http import  HttpResponse
+from django.shortcuts import render
 
-# def index(request):
-#     return HttpResponse('''<h2>Hello World</h2>
-#     <a href="https://www.youtube.com/watch?v=oXN2q7MjxGo">Song Time</a>''')
 def about(request):
     return HttpResponse(" About Hello World")
 
 def index(request):
-    return HttpResponse("Home")
+    params = {'name' : 'Neel', 'place': 'USA'}
+    return render(request, 'index.html', params)
+    # return HttpResponse("Home")
 def removepunc(request):
+    djtext = request.GET.get('text', 'default')
     return HttpResponse(" Remove Punc")
 def capfirst(request):
     return HttpResponse("Capitalize First")
 def newlineremove(request):
     return HttpResponse("New Line Remove")
 def spacefirst(request):
-    return HttpResponse("Space First")
+    return HttpResponse("Space First <a href='/'>back</a>")
 def charcount(request):
     return HttpResponse("Char Counnt")
