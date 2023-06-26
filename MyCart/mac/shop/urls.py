fa58aae478ebbcf50ea1b29e1bf552c868613617
neel32 <boghanineel@gmail.com>
2023-06-26 18:07:11 +0530

    26-06

--- MyCart/mac/shop/urls.py
+++ MyCart/mac/shop/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mac project.
+URL configuration for shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+# from django.contrib import admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/',include('shop.urls')),
-    path('blog/',include('blog.urls')),
+    # path('admin/', admin.site.urls),
+    path('', views.index, name = "ShopHome"),
+    path('about/', views.index, name = "About Us"),
+    path('contact/', views.contact, name = "Contact Us"),
+    path('tracker/', views.tracker, name = "Tracking Status"),
+    path('search/', views.search, name = "Search"),
+    path('productview/', views.productview, name = "Product View"),
+    path('checkout/', views.checkout, name = "Checkout"),
 ]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -19,13 +19,13 @@ def ex1(request):
 
 def analyze(request):
     #Get the text
-    djtext = request.GET.get('text', 'default')
+    djtext = request.POST.get('text', 'default')
 
     # Check checkbox values
-    removepunc = request.GET.get('removepunc', 'off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
 
     #Check which checkbox is on
     if removepunc == "on":
@@ -35,38 +35,42 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif(fullcaps=="on"):
+    if(fullcaps=="on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
 
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif(extraspaceremover=="on"):
+    if(extraspaceremover=="on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not(djtext[index] == " " and djtext[index+1]==" "):
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif (newlineremover == "on"):
+    if (newlineremover == "on"):
         analyzed = ""
         for char in djtext:
-            if char != "\n":
+            if char != "\n" and char != "\r":
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
         # Analyze the text
-        return render(request, 'analyze.html', params)
     else:
         return HttpResponse("Error")
+    return render(request, 'analyze.html', params)
 
 # def capfirst(request):
 #     return HttpResponse("capitalize first")
