977b788f4088deed842ec243fca1ba3cd0c45345
neel32 <boghanineel@gmail.com>
2023-06-27 18:11:46 +0530

    27-06

--- MyCart/mac/shop/views.py
+++ MyCart/mac/shop/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n//4 + ceil((n/4) - (n//4))
+    params = {'no_of_slides' : nSlides,'range' : range(nSlides), 'product' : products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return HttpResponse("We are at about")
