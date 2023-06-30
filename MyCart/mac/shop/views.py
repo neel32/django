e786931d2ba1fa0f28c6e7d041fbb0ebbe761336
neel32 <boghanineel@gmail.com>
2023-06-30 17:41:21 +0530

    30-06

--- MyCart/mac/shop/views.py
+++ MyCart/mac/shop/views.py
@@ -1,32 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
+# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    # products= Product.objects.all()
-    # n= len(products)
-    # nSlides= n//4 + ceil((n/4)-(n//4))
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
 
     allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
     for cat in cats:
-        prod = Product.objects.filter(category = cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allProds.append([prod, range(1, nSlides), nSlides])
-    # allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
-    params={'allProds':allProds }
-    return render(request,"shop/index.html", params)
 
-
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name = name, email = email, phone = phone, desc = desc)
+        contact.save()
     return render(request, 'shop/contact.html')
 
 def tracker(request):
@@ -35,8 +45,12 @@ def tracker(request):
 def search(request):
     return render(request, 'shop/search.html')
 
-def productview(request):
-    return render(request, 'shop/prodView.html')
+def productView(request, myid):
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+
+
+    return render(request, 'shop/prodView.html', {'product':product[0]})
 
 def checkout(request):
     return render(request, 'shop/checkout.html')
